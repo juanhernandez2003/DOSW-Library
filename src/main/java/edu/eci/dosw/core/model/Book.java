09c8286c258a053cd2e3c8119691c3a4514ba50f
juanhernandez2003 <juan.hmoreno@mail.escuelaing.edu.co>
2026-03-20 21:24:19 -0500

    Implemencatión model (Book, Loan, User)

--- src/main/java/edu/eci/dosw/core/model/Book.java
+++ src/main/java/edu/eci/dosw/core/model/Book.java
@@ -1,7 +1,32 @@
 package edu.eci.dosw.core.model;
 
-public class Book{
+public class Book {
+    private String id;
     private String title;
     private String author;
-    private String ID;
+    private boolean available;
+    private int copies;
+
+    public Book(String id, String title, String author, int copies) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.copies = copies;
+        this.available = copies > 0;
+    }
+
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getAuthor() { return author; }
+    public void setAuthor(String author) { this.author = author; }
+
+    public boolean isAvailable() { return available; }
+    public void setAvailable(boolean available) { this.available = available; }
+
+    public int getCopies() { return copies; }
+    public void setCopies(int copies) { this.copies = copies; }
 }
--- src/main/java/edu/eci/dosw/core/model/Loan.java
+++ src/main/java/edu/eci/dosw/core/model/Loan.java
@@ -1,11 +1,40 @@
 package edu.eci.dosw.core.model;
 
-import java.util.Date;
+import java.time.LocalDate;
 
-public class Loan{
+public class Loan {
+    public enum Status { ACTIVE, RETURNED }
+
+    private String id;
     private Book book;
     private User user;
-    private Date loanDate;
-    private String status;
-    private Date returnDate;
+    private LocalDate loanDate;
+    private LocalDate returnDate;
+    private Status status;
+
+    public Loan(String id, Book book, User user, LocalDate loanDate) {
+        this.id = id;
+        this.book = book;
+        this.user = user;
+        this.loanDate = loanDate;
+        this.status = Status.ACTIVE;
+    }
+
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+
+    public Book getBook() { return book; }
+    public void setBook(Book book) { this.book = book; }
+
+    public User getUser() { return user; }
+    public void setUser(User user) { this.user = user; }
+
+    public LocalDate getLoanDate() { return loanDate; }
+    public void setLoanDate(LocalDate loanDate) { this.loanDate = loanDate; }
+
+    public LocalDate getReturnDate() { return returnDate; }
+    public void setReturnDate(LocalDate returnDate) { this.returnDate = returnDate; }
+
+    public Status getStatus() { return status; }
+    public void setStatus(Status status) { this.status = status; }
 }
