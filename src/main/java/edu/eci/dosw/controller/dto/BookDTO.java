c2a797dedcf1e33bdde05e74e9994343faa9ae54
juanhernandez2003 <juan.hmoreno@mail.escuelaing.edu.co>
2026-03-20 21:53:11 -0500

    Implementación de DTOs

--- src/main/java/edu/eci/dosw/controller/dto/BookDTO.java
+++ src/main/java/edu/eci/dosw/controller/dto/BookDTO.java
@@ -1,4 +1,16 @@
 package edu.eci.dosw.controller.dto;
 
 public class BookDTO {
+    private String title;
+    private String author;
+    private int copies;
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getAuthor() { return author; }
+    public void setAuthor(String author) { this.author = author; }
+
+    public int getCopies() { return copies; }
+    public void setCopies(int copies) { this.copies = copies; }
 }
