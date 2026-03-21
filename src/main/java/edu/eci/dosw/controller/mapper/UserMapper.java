c18e20dee52159225f3f8b6aace8b58bdcde232c
juanhernandez2003 <juan.hmoreno@mail.escuelaing.edu.co>
2026-03-20 21:59:59 -0500

    Implementación de mappers

--- src/main/java/edu/eci/dosw/controller/mapper/UserMapper.java
+++ src/main/java/edu/eci/dosw/controller/mapper/UserMapper.java
@@ -1,4 +1,14 @@
 package edu.eci.dosw.controller.mapper;
 
+import edu.eci.dosw.controller.dto.UserDTO;
+import edu.eci.dosw.core.model.User;
+import org.springframework.stereotype.Component;
+
+@Component
 public class UserMapper {
+    public UserDTO toDTO(User user) {
+        UserDTO dto = new UserDTO();
+        dto.setName(user.getName());
+        return dto;
+    }
 }
