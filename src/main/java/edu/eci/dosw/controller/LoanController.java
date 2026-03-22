dc813993135d28831a644cdaae1eb1dbe691f25c
juanhernandez2003 <juan.hmoreno@mail.escuelaing.edu.co>
2026-03-21 20:20:12 -0500

    Dependencia de swagger y actualización controllers

--- src/main/java/edu/eci/dosw/controller/LoanController.java
+++ src/main/java/edu/eci/dosw/controller/LoanController.java
@@ -3,6 +3,8 @@ package edu.eci.dosw.controller;
 import edu.eci.dosw.controller.dto.LoanDTO;
 import edu.eci.dosw.core.model.Loan;
 import edu.eci.dosw.core.service.LoanService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/loans")
+@Tag(name = "Loans", description = "Operaciones sobre préstamos")
 public class LoanController {
     private final LoanService loanService;
 
@@ -17,11 +20,13 @@ public class LoanController {
         this.loanService = loanService;
     }
 
+    @Operation(summary = "Crear un préstamo")
     @PostMapping
     public ResponseEntity<Loan> createLoan(@RequestBody LoanDTO dto) {
         return ResponseEntity.ok(loanService.createLoan(dto.getBookId(), dto.getUserId()));
     }
 
+    @Operation(summary = "Obtener todos los préstamos")
     @GetMapping
     public ResponseEntity<List<Loan>> getAllLoans() {
         return ResponseEntity.ok(loanService.getAllLoans());
