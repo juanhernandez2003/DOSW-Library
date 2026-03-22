4dea927331542458c757b18dfa478b4df7bbe20a
juanhernandez2003 <juan.hmoreno@mail.escuelaing.edu.co>
2026-03-21 19:54:29 -0500

    Añadir un DTO

--- src/main/java/edu/eci/dosw/controller/dto/BookDTO.java
+++ src/main/java/edu/eci/dosw/controller/dto/BookDTO.java
@@ -1,16 +1,10 @@
 package edu.eci.dosw.controller.dto;
 
+import lombok.Data;
+
+@Data
 public class BookDTO {
     private String title;
     private String author;
     private int copies;
-
-    public String getTitle() { return title; }
-    public void setTitle(String title) { this.title = title; }
-
-    public String getAuthor() { return author; }
-    public void setAuthor(String author) { this.author = author; }
-
-    public int getCopies() { return copies; }
-    public void setCopies(int copies) { this.copies = copies; }
 }
